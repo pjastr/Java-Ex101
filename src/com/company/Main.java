1a58fc61adef2e2234005136e17df4325b87e9d1
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-05 13:58:28 +0200

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int pierwszaLiczba = 10, drugaLiczba = 20, i, j, k;
+        System.out.println("pierwszaLiczba: " + pierwszaLiczba);
+        System.out.println("drugaLiczba: " + drugaLiczba);
+        System.out.println("zmienna i: " + i);
+        System.out.println("zmienna j: " + j);
+        System.out.println("zmienna k: " + k);
+    }
+}
